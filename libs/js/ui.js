9ff609a55dd059a67b8d4b24e8b303243c428106
nomi <nomisoul@gmail.com>
2013-03-30 02:48:01 +0900

    최종본
    2013.03.30

--- libs/js/ui.js
+++ libs/js/ui.js
@@ -1,14 +1,39 @@
-
-$(document).ready(function() {
-	$('#gnb').on('mouseenter keyup', function() {
-	    $('#2depth').removeClass("dnone");
-		return false;
+$(document).ready(function(){
+	$('#2depth').hide();
+	var gnb = $('#gnb');
+	var depth = $('#2depth');
+   	
+	gnb.bind('mouseenter mouseover',function(){
+		$(depth).slideDown('fast');
+	});
+   
+   	depth.bind('mouseleave',function() {
+		$(depth).fadeOut('fast');
 	});
 	
-	$('#2depth').on('mouseleave', function() {
-	    $('#2depth').addClass("dnone");   
+	gnb.find('li').mouseover(function(){
+		var thisNum = $(this).index()+1;
+				
+		if($('#2depth_img').hasClass('gnb_img_01')){
+			$('#2depth_img').removeClass().addClass('gnb_img_0'+thisNum);
+		};
 	});
-		    
-}); // document.ready
+	
+	gnb.find('li').mouseleave(function(){
+		$('#2depth_img').removeClass().addClass('gnb_img_01');
+	});
+	
+	depth.find('ul').mouseover(function(){
+		if($('#2depth_img').hasClass('gnb_img_01')){
+			$('#2depth_img').removeClass().addClass('gnb_img_0'+$(this).index());
+		};
+	});
+ 
+	depth.find('ul').mouseleave(function(){
+		$('#2depth_img').removeClass().addClass('gnb_img_01');
+	});
+});
+
+
 
 
